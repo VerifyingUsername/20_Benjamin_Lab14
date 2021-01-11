bbd2b51face0eb795e01b315e39bc56445d4c29b
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2021-01-11 22:55:55 +0800

    Initial commit

---
+++ Assets/Scripts/CapsuleYellow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CapsuleYellow : MonoBehaviour
+{
+    public GameObject YellowText;
+    private int yellowCounter;
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            yellowCounter++;
+
+            YellowText.GetComponent<Text>().text = "Yellow: " + yellowCounter;
+
+            audioSource.Play();
+        }
+    }
+}
